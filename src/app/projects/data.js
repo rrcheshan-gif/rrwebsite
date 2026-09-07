b3ba6157b30233560739700f7d0aac2337c78637
rrcheshan-gif <rrcheshan-gif@users.noreply.github.com>
2026-09-07 17:05:27 +0530

    feat(projects): add RRP folder images to Rathnapura rural roads project

--- src/app/projects/data.js
+++ src/app/projects/data.js
@@ -556,9 +556,10 @@ const projectsData = [
     id: 'project-30',
     type: 'completed', category: "roads",
     title: "Rehabilitation of 30.12km Rural Roads in Rathnapura",
+    heroImage: '/images/IMAGES/RRP/20240201_074339.jpg',
+    galleryImages: ['/images/IMAGES/RRP/20240201_074339.jpg', '/images/IMAGES/RRP/20240201_074620.jpg', '/images/IMAGES/RRP/20240201_074651.jpg', '/images/IMAGES/RRP/20240201_074941.jpg', '/images/IMAGES/RRP/Background image.jpg', '/images/IMAGES/RRP/IMG-20240202-WA0039.jpg', '/images/IMAGES/RRP/IMG-20240202-WA0040.jpg', '/images/IMAGES/RRP/IMG-20240202-WA0044.jpg', '/images/IMAGES/RRP/IMG-20240307-WA0035 (1).jpg', '/images/IMAGES/RRP/IMG-20240307-WA0045.jpg', '/images/IMAGES/RRP/IMG-20240307-WA0049.jpg', '/images/IMAGES/RRP/IMG-20240407-WA0013.jpg', '/images/IMAGES/RRP/IMG-20240407-WA0015.jpg'],
     status: "Completed",
     client: "Road Development Authority",
-    
     duration: "18 Months",
     year: 2025
   },
