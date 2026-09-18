94e079930c0f46d4bff08d5937d9915f8f57759d
rrcheshan-gif <rrcheshan-gif@users.noreply.github.com>
2026-09-18 21:18:45 +0530

    design: restore 'Construction (Pvt) Ltd' text next to the logo in navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -53,6 +53,10 @@ export default function Navbar() {
           <nav className="navbar">
             <a href="/" className="logo" onClick={() => setMobileMenuOpen(false)}>
               <img src="/images/logo.png" alt="RR Construction Logo" className="nav-logo-img" />
+                <div className="logo-text">
+                  <span className="logo-title">Construction</span>
+                  <span className="logo-sub">(Pvt) Ltd</span>
+                </div>
               
             </a>
             
