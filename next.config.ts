055aa2e4fc4e97627367e032f580092ad3df7f55
rrcheshan-gif <rrcheshan-gif@users.noreply.github.com>
2026-09-23 08:24:04 +0530

    sec: add http security headers to next.js config

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,42 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
 
+  
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          }
+        ],
+      },
+    ]
+  },
+
   async redirects() {
     return [
       // Old About URLs → New
